951a2ad8108ced46c7aa260c226f5a9df4052cd9
Shirash <devilvenom1010@gmail.com>
2026-02-26 10:01:48 +0545

    versioning 2

---
+++ Database Project/Tables/ClientDeploymentHistory.sql
@@ -0,0 +1,13 @@
+﻿-- 1. Create the new History table
+CREATE TABLE dbo.ClientDeploymentHistory
+(
+    HistoryId INT IDENTITY(1,1) PRIMARY KEY,
+    ClientId INT NOT NULL,
+    DeployedBy VARCHAR(100) NOT NULL,
+    DeployedAt DATETIME NOT NULL DEFAULT GETDATE(),
+    DeployStatus VARCHAR(50) NOT NULL,
+    VersionNumber VARCHAR(50) NULL,
+    ErrorMessage NVARCHAR(MAX) NULL,
+    ActiveVersion VARCHAR(50) NULL
+
+);
